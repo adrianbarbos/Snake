86543d0df3f41f31e46c25c2f79ccfe753305fd8
Delta <delta@Deltas-Mac.local>
2014-07-18 15:49:45 +0300

    Initial Commit

---
+++ Snake/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Snake
+//
+//  Created by Delta on 7/18/14.
+//  Copyright (c) 2014 AKA Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Snake/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Snake
+//
+//  Created by Delta on 7/18/14.
+//  Copyright (c) 2014 AKA Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
